2e2bcbc7ad139fc82eabb8350865e154e69df664
m.ostanin <ostanin.m.13@gmail.com>
2025-08-20 11:42:56 +0300

    feat: Add velocity and acceleration limit editors to Robot UI

--- Assets/TrajectoryPlanning/UI/RobotUIPresenter.cs
+++ Assets/TrajectoryPlanning/UI/RobotUIPresenter.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Linq;
 using MathNet.Numerics.LinearAlgebra;
 using TrajectoryPlanning.Robot;
 using UniRx;
+using UnityEngine;
+using UnityEngine.UIElements;
 using Zenject;
 
 namespace TrajectoryPlanning.UI
@@ -24,7 +27,49 @@ namespace TrajectoryPlanning.UI
         {
             _view.SetModel(_robotModel.Id, _robotModel.Dof);
             _view.BuildJoints(_robotModel.JointNames, _robotModel.Dof);
+            _view.BuildLimitEditors(_robotModel.JointNames, _robotModel.Dof);
             _robotModel.CurrentState.Subscribe(UpdateState).AddTo(_disposables);
+            _robotModel
+                .JointsMaxVelocity.Subscribe(v => _view.SetVelocityLimitsDegPerSec(v))
+                .AddTo(_disposables);
+            _robotModel
+                .JointsMaxAcceleration.Subscribe(v => _view.SetAccelerationLimitsDegPerSec2(v))
+                .AddTo(_disposables);
+
+            _view.SetVelocityLimitsDegPerSec(_robotModel.JointsMaxVelocity.Value);
+            _view.SetAccelerationLimitsDegPerSec2(_robotModel.JointsMaxAcceleration.Value);
+
+            _view.ForEachVelocityLimitField(
+                (i, field) =>
+                {
+                    EventCallback<ChangeEvent<float>> handler = evt =>
+                    {
+                        var current = _robotModel.JointsMaxVelocity.Value.ToArray();
+                        current[i] = evt.newValue * Mathf.Deg2Rad;
+                        _robotModel.SetVelocityLimits(current);
+                    };
+                    field.RegisterValueChangedCallback(handler);
+                    _disposables.Add(
+                        Disposable.Create(() => field.UnregisterValueChangedCallback(handler))
+                    );
+                }
+            );
+
+            _view.ForEachAccelerationLimitField(
+                (i, field) =>
+                {
+                    EventCallback<ChangeEvent<float>> handler = evt =>
+                    {
+                        var current = _robotModel.JointsMaxAcceleration.Value.ToArray();
+                        current[i] = evt.newValue * Mathf.Deg2Rad;
+                        _robotModel.SetAccelerationLimits(current);
+                    };
+                    field.RegisterValueChangedCallback(handler);
+                    _disposables.Add(
+                        Disposable.Create(() => field.UnregisterValueChangedCallback(handler))
+                    );
+                }
+            );
             _view
                 .MoveButton.OnClickAsObservable()
                 .Subscribe(_ => MoveToTargets())
--- Assets/TrajectoryPlanning/UI/RobotView.cs
+++ Assets/TrajectoryPlanning/UI/RobotView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using MathNet.Numerics.LinearAlgebra;
 using UnityEngine;
@@ -11,10 +12,14 @@ namespace TrajectoryPlanning.UI
         public Label DofLabel { get; }
         public ScrollView JointsScroll { get; }
         public ScrollView TargetsScroll { get; }
+        public ScrollView VelocityLimitsScroll { get; }
+        public ScrollView AccelerationLimitsScroll { get; }
         public Button MoveButton { get; }
 
         private readonly List<Label> _jointValueLabels = new();
         private readonly List<FloatField> _jointInputFields = new();
+        private readonly List<FloatField> _velocityLimitFields = new();
+        private readonly List<FloatField> _accelerationLimitFields = new();
 
         public RobotView(UIDocument document)
         {
@@ -23,7 +28,61 @@ namespace TrajectoryPlanning.UI
             DofLabel = root.Q<Label>("DofLabel");
             JointsScroll = root.Q<ScrollView>("JointsScroll");
             TargetsScroll = root.Q<ScrollView>("TargetsScroll");
+            VelocityLimitsScroll = root.Q<ScrollView>("VelocityLimitsScroll");
+            AccelerationLimitsScroll = root.Q<ScrollView>("AccelerationLimitsScroll");
             MoveButton = root.Q<Button>("MoveButton");
+
+            var stateSection = root.Q<VisualElement>("StateSection");
+            var targetsSection = root.Q<VisualElement>("TargetsSection");
+            var velocitySection = root.Q<VisualElement>("VelocitySection");
+            var accelerationSection = root.Q<VisualElement>("AccelerationSection");
+
+            var stateTab = root.Q<Toggle>("StateTab");
+            var targetsTab = root.Q<Toggle>("TargetsTab");
+            var velocityTab = root.Q<Toggle>("VelocityTab");
+            var accelerationTab = root.Q<Toggle>("AccelerationTab");
+
+            stateTab.RegisterValueChangedCallback(evt =>
+            {
+                if (evt.newValue)
+                    ShowSection("State");
+            });
+            targetsTab.RegisterValueChangedCallback(evt =>
+            {
+                if (evt.newValue)
+                    ShowSection("Targets");
+            });
+
+            velocityTab.RegisterValueChangedCallback(evt =>
+            {
+                if (evt.newValue)
+                    ShowSection("Velocity");
+            });
+            accelerationTab.RegisterValueChangedCallback(evt =>
+            {
+                if (evt.newValue)
+                    ShowSection("Acceleration");
+            });
+
+            ShowSection("State");
+            return;
+
+            void ShowSection(string which)
+            {
+                stateSection.style.display =
+                    which == "State" ? DisplayStyle.Flex : DisplayStyle.None;
+                targetsSection.style.display =
+                    which == "Targets" ? DisplayStyle.Flex : DisplayStyle.None;
+                velocitySection.style.display =
+                    which == "Velocity" ? DisplayStyle.Flex : DisplayStyle.None;
+                accelerationSection.style.display =
+                    which == "Acceleration" ? DisplayStyle.Flex : DisplayStyle.None;
+
+                stateTab.SetValueWithoutNotify(which == "State");
+                targetsTab.SetValueWithoutNotify(which == "Targets");
+                velocityTab.SetValueWithoutNotify(which == "Velocity");
+                accelerationTab.SetValueWithoutNotify(which == "Acceleration");
+            }
         }
 
         public void SetModel(string modelId, int dof)
@@ -71,6 +130,72 @@ namespace TrajectoryPlanning.UI
                 TargetsScroll.Add(targetRow);
                 _jointInputFields.Add(input);
             }
+        }
+
+        public void BuildLimitEditors(string[] jointNames, int dof)
+        {
+            VelocityLimitsScroll.Clear();
+            AccelerationLimitsScroll.Clear();
+            _velocityLimitFields.Clear();
+            _accelerationLimitFields.Clear();
+
+            var count = Mathf.Min(jointNames.Length, dof);
+            for (var i = 0; i < count; i++)
+            {
+                var velRow = new VisualElement();
+                velRow.AddToClassList("row");
+                var velName = new Label(jointNames[i]);
+                velName.AddToClassList("jointName");
+                var velField = new FloatField();
+                velField.AddToClassList("jointInput");
+                velRow.Add(velName);
+                velRow.Add(velField);
+                VelocityLimitsScroll.Add(velRow);
+                _velocityLimitFields.Add(velField);
+
+                var accRow = new VisualElement();
+                accRow.AddToClassList("row");
+                var accName = new Label(jointNames[i]);
+                accName.AddToClassList("jointName");
+                var accField = new FloatField();
+                accField.AddToClassList("jointInput");
+                accRow.Add(accName);
+                accRow.Add(accField);
+                AccelerationLimitsScroll.Add(accRow);
+                _accelerationLimitFields.Add(accField);
+            }
+        }
+
+        public void SetVelocityLimitsDegPerSec(float[] valuesRadPerSec)
+        {
+            var count = Mathf.Min(valuesRadPerSec.Length, _velocityLimitFields.Count);
+            for (var i = 0; i < count; i++)
+            {
+                var deg = valuesRadPerSec[i] * Mathf.Rad2Deg;
+                _velocityLimitFields[i].SetValueWithoutNotify(deg);
+            }
+        }
+
+        public void SetAccelerationLimitsDegPerSec2(float[] valuesRadPerSec2)
+        {
+            var count = Mathf.Min(valuesRadPerSec2.Length, _accelerationLimitFields.Count);
+            for (var i = 0; i < count; i++)
+            {
+                var deg = valuesRadPerSec2[i] * Mathf.Rad2Deg;
+                _accelerationLimitFields[i].SetValueWithoutNotify(deg);
+            }
+        }
+
+        public void ForEachVelocityLimitField(Action<int, FloatField> action)
+        {
+            for (var i = 0; i < _velocityLimitFields.Count; i++)
+                action(i, _velocityLimitFields[i]);
+        }
+
+        public void ForEachAccelerationLimitField(Action<int, FloatField> action)
+        {
+            for (var i = 0; i < _accelerationLimitFields.Count; i++)
+                action(i, _accelerationLimitFields[i]);
         }
 
         public void UpdateState(Vector<float> state)
